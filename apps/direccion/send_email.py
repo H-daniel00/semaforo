ae95c2ad86546c7f1d36be644f04fc15db1f8314
daviddc <dedc_011596@hotmail.com>
2019-10-11 09:31:43 -0500

    plantilla de notificación email v1

--- apps/direccion/send_email.py
+++ apps/direccion/send_email.py
@@ -1,16 +1,17 @@
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from .models import Correos_Notificacion
 
 def send_notification_mail(from_user, to_user):
     if to_user is not None and from_user is not None:
         if Correos_Notificacion.objects.filter(usuario = to_user).exists():
-            print(to_user.correos)
             send_mail(
                 'Notificación de Control Sedec',
-                'Se le ha asignado una nueva actividad por ' + from_user.get_full_name(),
-                'davidernesto@tabasco.gob.mx',
+                '',
+                'controlsedec@tabasco.gob.mx',
                 [to_user.correos.correo_inst, to_user.correos.correo_per],
                 fail_silently=False,
+                html_message= render_to_string('base/nueva_actividad_email.html', {'from_user': from_user.get_full_name, 'to_user': to_user.get_full_name}),
             )
         else:
             print('no tiene correos')
